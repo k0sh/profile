098ff09438599edc37aeebd6c37333019ecf3a30
Renat <renatn@gmail.com>
2009-11-11 18:09:52 +0300

    Fixed support dbus

--- bin/homepro.sh
+++ bin/homepro.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-SFILES=`git --git-dir /home/renat/.git --work-tree /home/renat status | grep "modified:" | sed 's/^.*modified://g'`
+SFILES=`/usr/bin/git --git-dir /home/renat/.git --work-tree /home/renat status | /bin/grep "modified:" | /bin/sed 's/^.*modified://g'`
 NFILES=`echo "$SFILES" | wc -l`
+
+NOTIFY_ENV=`ps -e e | grep "/usr/libexec/notification-daemon"` 
+DBUS_SESSION_ENV=`echo "$NOTIFY_ENV" | grep -E -o "DBUS_SESSION_BUS_ADDRESS=unix:abstract=/tmp/dbus-.*guid=[0-9A-Za-z]{32}" | head -n 1`
+DBUS_SESSION_BUS_ADDRESS=`echo "$DBUS_SESSION_ENV" | sed "s/^DBUS_SESSION_BUS_ADDRESS=//g"`
+export DBUS_SESSION_BUS_ADDRESS
+
 if [ $NFILES -gt 0 ]; then
-	echo "Modified $NFILES files"
 	/usr/bin/notify-send -u normal -i gnome-note "There are $NFILES modified files " "$SFILES"
 fi
