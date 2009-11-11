82bd34c5ec4b76633eed7eb406df12168ec4ac91
Renat <renatn@gmail.com>
2009-11-11 19:21:30 +0300

    Added check if sfiles is empty

--- bin/homepro.sh
+++ bin/homepro.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 SFILES=`/usr/bin/git --git-dir /home/renat/.git --work-tree /home/renat status | /bin/grep "modified:" | /bin/sed 's/^.*modified://g'`
-NFILES=`echo "$SFILES" | wc -l`
+[ -z $SFILES ] && exit
 
+NFILES=`echo "$SFILES" | wc -l`
 NOTIFY_ENV=`ps -e e | grep "/usr/libexec/notification-daemon"` 
 DBUS_SESSION_ENV=`echo "$NOTIFY_ENV" | grep -E -o "DBUS_SESSION_BUS_ADDRESS=unix:abstract=/tmp/dbus-.*guid=[0-9A-Za-z]{32}" | head -n 1`
 DBUS_SESSION_BUS_ADDRESS=`echo "$DBUS_SESSION_ENV" | sed "s/^DBUS_SESSION_BUS_ADDRESS=//g"`
