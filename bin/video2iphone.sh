f3519ae0b1f0aa0ffbeef6c1a135b29eb4c8dd11
Renat <renatn@fedora.local>
2009-12-12 22:01:59 +0300

    Added video2iphone script based on ffmpeg

---
+++ bin/video2iphone.sh
@@ -0,0 +1,21 @@
+:#!/bin/sh
+
+VBR=296k          #полнимаем для лучшего видео
+ABR=64k           #поднимаем для лучшего звука
+FRAME=15          #25 для MPlayer ;)
+VCODEC=libxvid    #или mpeg4
+ACODEC=aac        #в некоторых версиях ffmpeg - libfaac
+VIDEOSIZE=480x320 #no comments
+CONTAINER=mp4     #можно менять на 3gp
+
+SRC=$1
+if [ "$2" == "" ]; then
+    DST=$SRC.$CONTAINER
+else
+    DST=$2
+fi
+
+ffmpeg -y -i "$SRC" -s $VIDEOSIZE -aspect 4:3 -vcodec $VCODEC -b $VBR -r $FRAME "$DST" -pass 1 -passlogfile passlogfile.log
+ffmpeg -y -i "$SRC" -s $VIDEOSIZE -aspect 4:3 -vcodec $VCODEC -b $VBR -r $FRAME -acodec $ACODEC -ac 1 -ab $ABR "$DST" -pass 2 -passlogfile passlogfile.log
+
+
